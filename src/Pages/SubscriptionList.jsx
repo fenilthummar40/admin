424cedd6fc82ac7767a77f8f8f694a541ddca3d0
fenil thummar <152170831+fenilthummar40@users.noreply.github.com>
2026-03-14 14:57:56 +0530

    SubscriptionList

--- src/Pages/SubscriptionList.jsx
+++ src/Pages/SubscriptionList.jsx
@@ -14,7 +14,7 @@ function SubscriptionList() {
     const fetchSubscription = async () => {
         try {
             const response = await axios.get(
-                "https://backend-uaa2.onrender.com/api/subscription/list"
+                "https://backend-uaa2.onrender.com/api/subscription/list" 
             );
 
             if (response.data.success) {
