1a7bb6f0ec593b9c52cb68d308b6ad7fd34ccb25
fenil thummar <152170831+fenilthummar40@users.noreply.github.com>
2026-03-16 14:41:29 +0530

    ContactList

--- src/Pages/ContactList.jsx
+++ src/Pages/ContactList.jsx
@@ -39,46 +39,44 @@ function ContactList() {
                         </div>
 
                         <div className='border w-full p-5 dark:border-secondary'>
-                            {contact.length > 0 ? (
-                                contact.map((item) => (
-                                    <div key={item._id} className="mb-4 pb-2">
-                                        <div className="border rounded-lg overflow-hidden shadow-sm dark:border-secondary overflow-x-auto overflow-y-auto">
-                                            <table className="w-full text-sm text-left border-collapse">
-                                                <thead>
-                                                <tr className="bg-secondary/20 text-gray-700">
-                                                    <th className="p-4 font-semibold dark:text-white">First Name</th>
-                                                    <th className="p-4 font-semibold dark:text-white">Last Name</th>
-                                                    <th className="p-4 font-semibold dark:text-white">Email</th>
-                                                    <th className="p-4 font-semibold dark:text-white">Phone</th>
-                                                </tr>
-                                                </thead>
+                            <div className="border rounded-lg overflow-hidden shadow-sm dark:border-secondary overflow-x-auto">
 
-                                                <tbody>
-                                                {contact.length > 0 ? (
-                                                    contact.map((item) => (
-                                                        <tr key={item._id}
-                                                            className="border-t hover:bg-gray-50 transition duration-200 dark:border-secondary">
-                                                            <td className="p-4 dark:text-light">{item.first_name}</td>
-                                                            <td className="p-4 dark:text-light">{item.last_name}</td>
-                                                            <td className="p-4 dark:text-light">{item.email}</td>
-                                                            <td className="p-4 dark:text-light">{item.phone}</td>
-                                                        </tr>
-                                                    ))
-                                                ) : (
-                                                    <tr>
-                                                        <td colSpan="4" className="text-center p-6 text-gray-500">
-                                                            No Contacts Found
-                                                        </td>
-                                                    </tr>
-                                                )}
-                                                </tbody>
-                                            </table>
-                                        </div>
-                                    </div>
-                                ))
-                            ) : (
-                                <p>No Contacts Found</p>
-                            )}
+                                <table className="w-full text-sm text-left border-collapse">
+
+                                    <thead>
+                                    <tr className="bg-secondary/20 text-gray-700">
+                                        <th className="p-4 font-semibold dark:text-white">First Name</th>
+                                        <th className="p-4 font-semibold dark:text-white">Last Name</th>
+                                        <th className="p-4 font-semibold dark:text-white">Email</th>
+                                        <th className="p-4 font-semibold dark:text-white">Phone</th>
+                                    </tr>
+                                    </thead>
+
+                                    <tbody>
+                                    {contact.length > 0 ? (
+                                        contact.map((item) => (
+                                            <tr
+                                                key={item._id}
+                                                className="border-t hover:bg-gray-50 transition duration-200 dark:border-secondary"
+                                            >
+                                                <td className="p-4 dark:text-light">{item.first_name}</td>
+                                                <td className="p-4 dark:text-light">{item.last_name}</td>
+                                                <td className="p-4 dark:text-light">{item.email}</td>
+                                                <td className="p-4 dark:text-light">{item.phone}</td>
+                                            </tr>
+                                        ))
+                                    ) : (
+                                        <tr>
+                                            <td colSpan="4" className="text-center p-6 text-gray-500">
+                                                No Contacts Found
+                                            </td>
+                                        </tr>
+                                    )}
+                                    </tbody>
+
+                                </table>
+
+                            </div>
                         </div>
                     </div>
                 </div>
